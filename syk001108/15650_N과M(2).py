d6a99bac201328c55ef75b55aadec0ad94397cd3
Suyeon Kim <57708995+syk001108@users.noreply.github.com>
2025-01-10 21:03:47 +0900

    Create 15650_N과M(2).py

---
+++ syk001108/15650_N과M(2).py
@@ -0,0 +1,13 @@
+n, m = map(int, input().split())
+sequence = []
+def dfs(start):
+    if len(sequence) == m:
+        print(' '.join(map(str, sequence)))
+        return
+    
+    for i in range(start, n + 1):
+        sequence.append(i)
+        dfs(i + 1)
+        sequence.pop()
+        
+dfs(1)
