2a745d25483c325b9af6a121d198698334a85a33
Suyeon Kim <57708995+syk001108@users.noreply.github.com>
2025-02-14 22:15:41 +0900

    Create 15656_N과M(7).py

---
+++ syk001108/15656_N과M(7).py
@@ -0,0 +1,13 @@
+n, m = map(int, input().split())
+numbers = sorted(list(map(int, input().split())))
+sequence = []
+
+def dfs(sequence):
+    if len(sequence) == m:
+        print(' '.join(map(str, sequence)))
+        return
+    
+    for i in numbers:
+        dfs(sequence + [i])
+            
+dfs([])
