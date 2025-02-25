244f66dd7a9da4d38b9ed16fdff0215989220478
kwang il <57484942+kamothi@users.noreply.github.com>
2025-02-25 21:21:22 +0900

    Update 11479_서로다른부분문자열개수.java

--- kamothi/11479_서로다른부분문자열개수.java
+++ kamothi/11479_서로다른부분문자열개수.java
@@ -1,13 +1,13 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 // 서로다른 부분 문자열의 개수
 public class boj_11478 {
     static String s;
-    static int result = 0;
-    static HashMap<String, Integer> h = new HashMap<>();
+    static Set<String> hs = new HashSet<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -16,14 +16,14 @@ public class boj_11478 {
 
         for (int i = 1; i <= s.length(); i++) solve(i);
 
-        System.out.println(h.size());
+        System.out.println(hs.size());
 
     }
 
     public static void solve(int n){
         for (int i = 0; i <= s.length() - n; i++) {
             String sub = s.substring(i, i + n);
-            if (!h.containsKey(sub))  h.put(sub,1);
+            hs.add(sub);
         }
     }
 }
