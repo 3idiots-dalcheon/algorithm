e9815148074763d3e1ec76d75cfae2ade969c3db
Suyeon Kim <57708995+syk001108@users.noreply.github.com>
2025-04-25 21:52:14 +0900

    Create 프로그래머스_최솟값만들기(LV2).js

---
+++ syk001108/프로그래머스_최솟값만들기.js
@@ -0,0 +1,10 @@
+function solution(A, B) {
+    var answer = 0;
+    A.sort((a, b) => a - b); // 오름차순 정렬
+    B.sort((a, b) => b - a); // 내림차순 정렬
+    
+    for (let i = 0; i < A.length; i++) {
+        answer += A[i] * B[i];
+    }
+    return answer;
+}
